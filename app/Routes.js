c6656d9df913ead5bf77e260d1bf92aca291ba60
chensheng <cson_chensheng@163.com>
2019-09-19 16:25:23 +0800

    fix: fix exact routers

--- app/Routes.js
+++ app/Routes.js
@@ -4,12 +4,14 @@ import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import TodoListPage from './containers/TodoListPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
+      <Route exact path={routes.TODOLIST} component={TodoListPage} />
+      <Route exact path={routes.COUNTER} component={CounterPage} />
+      <Route exact path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
 );
---
+++ app/containers/TodoListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class TodoListPage extends React.Component {
+  render() {
+    return <div>todolist...</div>;
+  }
+}
