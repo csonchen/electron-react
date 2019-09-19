5b30ec3fe034fb88cf22eb03afab690c21b166cf
chensheng <cson_chensheng@163.com>
2019-09-19 18:37:55 +0800

    add antd css

--- app/containers/TodoListPage.js
+++ app/containers/TodoListPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Search from 'antd/lib/input/Search';
 
 export default class TodoListPage extends React.Component {
   render() {
-    return <div>todolist...</div>;
+    return (
+      <div>
+        <Search
+          placeholder="input search text"
+          enterButton="Search"
+          size="large"
+          onSearch={value => console.log(value)}
+        />
+      </div>
+    );
   }
 }
